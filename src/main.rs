ecd8d00d98cd8a89f6690b7428060a27500c4af9
Aphomer <aaphomer@gmail.com>
2024-12-31 07:57:57 +0100

    Initialozing rust

---
+++ src/main.rs
@@ -0,0 +1,108 @@
+// use rand::Rng;
+// use std::cmp::Ordering;
+// use std::io;
+
+// fn main() {
+//     println!("Guess the number!");
+
+//     let secret_number = rand::thread_rng().gen_range(1..=100);
+
+//     // println!("The secret number is: {secret_number}");
+
+//     loop {
+//         println!("Please input your guess.");
+
+//         let mut guess = String::new();
+
+//         // --snip--
+
+//         io::stdin()
+//             .read_line(&mut guess)
+//             .expect("Failed to read line");
+
+//         let guess: u32 = match guess.trim().parse() {
+//             Ok(num) => num,
+//             Err(_) => continue,
+//         };
+
+//         println!("You guessed: {guess}");
+
+//         // --snip--
+
+//         match guess.cmp(&secret_number) {
+//             Ordering::Less => println!("Too small!"),
+//             Ordering::Greater => println!("Too big!"),
+//             Ordering::Equal => {
+//                 println!("You win!");
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+
+
+
+// println!("DATA STRUCTURE.");
+
+
+// use std::io;
+
+// fn main() {
+
+    
+//     println!("DATA STRUCTURE.");
+
+//     let a = [1, 2, 3, 4, 5];
+
+//     println!("Please enter an array index.");
+
+//     let mut index = String::new();
+
+//     io::stdin()
+//         .read_line(&mut index)
+//         .expect("Failed to read line");
+
+//     let index: usize = index
+//         .trim()
+//         .parse()
+//         .expect("Index entered was not a number");
+
+//     let element = a[index];
+//     let double_element = a[index] * 2;
+
+//     println!("The value of the element at index {index} is: {element}");
+//     println!("The DOUBLE value of the element at index {index} is: {double_element}");
+// }
+
+
+// fn main () {
+
+//     square_of_x(4)
+// }
+
+// fn square_of_x(x: i32) {
+//     println!("the Square of x is {x}*{x}");
+// }
+
+
+// fn main() {
+//     print_labeled_measurement(5, 'h');
+// }
+
+// fn print_labeled_measurement(value: i32, unit_label: char) {
+//     println!("The measurement is: '{value}{unit_label}'");
+// }
+
+fn main() {
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    s.push_str(", I am Aphomer");
+    s.push_str(", of God !");
+    println!("{s}"); // This will print `hello, world!`
+}
+
+
+
